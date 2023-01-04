70ba57dafdd4b363b9920516264f719b4b1933fa
Gerard Downes <gdownes@gmail.com>
2023-01-04 23:45:24 +0000

    Add pre and post

---
+++ setup.js
@@ -0,0 +1,9 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const context = JSON.stringify(github.context, undefined, 2)
+    console.log(`The pre event context: ${context}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
